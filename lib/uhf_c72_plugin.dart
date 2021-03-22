03fe5bd0519a409cfebb931c869c0e6e3788228b
hamidvs <acer@LAPTOP-NR6CNSBC>
2021-03-22 20:15:46 +0400

    Migrated to null safety

--- lib/uhf_c72_plugin.dart
+++ lib/uhf_c72_plugin.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class UhfC72Plugin {
   static const MethodChannel _channel = const MethodChannel('uhf_plugin');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
@@ -14,48 +14,48 @@ class UhfC72Plugin {
       EventChannel('ConnectedStatus');
   static const EventChannel tagsStatusStream = EventChannel('TagsStatus');
 
-  static Future<bool> get isStarted async {
+  static Future<bool?> get isStarted async {
     return _channel.invokeMethod('isStarted');
   }
 
-  static Future<bool> get startSingle async {
+  static Future<bool?> get startSingle async {
     return _channel.invokeMethod('startSingle');
   }
 
-  static Future<bool> get startContinuous async {
+  static Future<bool?> get startContinuous async {
     return _channel.invokeMethod('startContinuous');
   }
 
-  static Future<bool> get stop async {
+  static Future<bool?> get stop async {
     return _channel.invokeMethod('stop');
   }
 
-  static Future<bool> get close async {
+  static Future<bool?> get close async {
     return _channel.invokeMethod('close');
   }
 
-  static Future<bool> get clearData async {
+  static Future<bool?> get clearData async {
     return _channel.invokeMethod('clearData');
   }
 
-  static Future<bool> get isEmptyTags async {
+  static Future<bool?> get isEmptyTags async {
     return _channel.invokeMethod('isEmptyTags');
   }
 
-  static Future<bool> get connect async {
+  static Future<bool?> get connect async {
     return _channel.invokeMethod('connect');
   }
 
-  static Future<bool> get isConnected async {
+  static Future<bool?> get isConnected async {
     return _channel.invokeMethod('isConnected');
   }
 
-  static Future<bool> setPowerLevel(String value) async {
+  static Future<bool?> setPowerLevel(String value) async {
     return _channel
         .invokeMethod('setPowerLevel', <String, String>{'value': value});
   }
 
-  static Future<bool> setWorkArea(String value) async {
+  static Future<bool?> setWorkArea(String value) async {
     return _channel
         .invokeMethod('setWorkArea', <String, String>{'value': value});
   }
